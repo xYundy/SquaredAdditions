583a6cbb493b83b2cb588ba3efe120c5e22dc1c4
Wojciech Zięciak <wojciech.zieciak@itplanet.pl>
2024-03-01 00:07:06 +0100

    Initial project commit

---
+++ src/client/java/pl/xyundy/squaredadditions/SquaredAdditionsClient.java
@@ -0,0 +1,10 @@
+package pl.xyundy.squaredadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SquaredAdditionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/pl/xyundy/squaredadditions/SquaredAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package pl.xyundy.squaredadditions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SquaredAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
