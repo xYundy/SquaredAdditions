3931fdfbcff6a88b97710a1f4ffa5ec6b5aafe94
Wojciech Zięciak <wojciech.zieciak@gmail.com>
2024-03-06 19:34:07 +0100

    Changed log info for testing

--- src/main/java/pl/xyundy/squaredadditions/SquaredAdditions.java
+++ src/main/java/pl/xyundy/squaredadditions/SquaredAdditions.java
@@ -17,6 +17,6 @@ public class SquaredAdditions implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("SquaredAdditions initialized!");
 	}
 }
