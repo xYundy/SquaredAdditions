8db7d2f0c8721b8c128dd285dce7fb41c2a2f618
Wojciech Zięciak <wojciech.zieciak@itplanet.pl>
2024-04-13 00:34:30 +0200

    Add ModArmorMaterial enum boilerplate

---
+++ src/main/java/pl/xyundy/squaredadditions/item/ModArmorMaterial.java
@@ -0,0 +1,51 @@
+package pl.xyundy.squaredadditions.item;
+
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+
+public enum ModArmorMaterial implements ArmorMaterial {
+    ;
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return 0;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}
